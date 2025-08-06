bd9dc03677821adcc6b133bfa12bb0c92c09164c
admlpires <pires.leandro.deoliveira@gmail.com>
2025-08-06 13:47:20 -0300

    Corrige rotas para as views corretas

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,17 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+// Rota para a Página de Obituário
+Route::get('/obituario', function () {
+    return view('obituario'); 
+});
+
+// Rota para a Página de Venda Programada
+Route::get('/venda-programada', function () {
+    return view('venda-programada');
 });
 
 Route::get('/dashboard', function () {
